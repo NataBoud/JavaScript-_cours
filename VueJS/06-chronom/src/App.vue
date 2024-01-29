36f125623b7fdcbfa639b8d368aa64b11593e581
Natalia Boudard <natalia.boudard@gmail.com>
2024-01-29 16:32:54 +0100

    chrono

---
+++ VueJS/06-chronom/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import MinuteurDisplay from './components/MinuteurDisplay.vue';
+
+
+</script>
+
+<template>
+  <h1>Minuteur</h1>
+  <MinuteurForm/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ VueJS/06-chronom/src/components/MinuteurForm.vue
@@ -0,0 +1,34 @@
+<!-- <script setup>
+import { reactive } from "vue";
+
+const minuteurForm = reactive({
+    minutes: 0,
+    seconds: 0,
+}) ;
+
+const emits = defineEmits(["ajouterMinumeur"])
+const submitHandler = () => {
+    emits("ajouterMinumeur", {
+        ...minuteurForm
+    })
+}
+</script>
+
+<template>
+    <form action="#" @submit.prevent="submitHandler">
+        <div>
+            <label for="minutes">Minutes: </label>
+            <input v-model="minuteurForm.minutes" type="number" min="0" max="59" required>
+        </div>
+
+        <div>
+            <label for="seconds">Seconds: </label>
+            <input v-model="minuteurForm.seconds" type="number" min="0" max="59" required>
+        </div>
+        <button>Ajouter un minuteur</button>
+    </form>
+</template>
+
+<style scoped>
+
+</style> -->
