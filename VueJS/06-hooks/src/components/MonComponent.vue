98b0598b70a27ebc44cf9e2a8b6c7d45665fabe1
Natalia Boudard <natalia.boudard@gmail.com>
2024-02-01 08:43:11 +0100

    hooks

---
+++ VueJS/06-hooks/src/components/MonComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+
+
+onMounted(() => {
+    console.log('onMounted');
+})
+
+onUnmounted(() => {
+    console.log('onUnmounted');
+})
+
+</script>
+
+<template>
+    <p>Mon paragraphe</p>
+</template>
+
+<style scoped>
+
+</style>
