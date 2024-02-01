e8466bcd7015475c8b818266356743d06113c1c7
Natalia Boudard <natalia.boudard@gmail.com>
2024-02-01 17:18:17 +0100

    routes

---
+++ VueJS/exercice-routes/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { createRouter } from 'vue-router'
+import About from './About.vue';
+import Home from './Home.vue';
+import Contact from './Contact.vue';
+
+const routes = [
+    {path: "/", component: Home},
+    {path: "/about", component: About},
+    {path: "/contact", component: Contact}
+]
+
+const router = createRouter({routes})
+
+</script>
+
+<template>
+    <div>
+        <router-link to="/">Accueil</router-link>
+        <router-link to="/about">A propos</router-link>
+        <router-link to="/contact">Contact</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
